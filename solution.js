8fde3532a97fb21cdb9c3d608851e8b32eb296a3
Rami B <ramibasheer9@gmail.com>
2022-07-11 02:20:42 +0200

    submitting solutions

---
+++ solution.js
@@ -0,0 +1,12 @@
+
+let introSentence = "hello, my name is Rami, I am";
+let age = 25;
+console.log(introSentence + " " + age + ' years old');
+
+
+
+let integer = "1005";
+console.log(parseInt(integer));
+
+let float = "10.05";
+console.log(parseFloat(float));
